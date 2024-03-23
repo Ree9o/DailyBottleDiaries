d7ede767c69e09af59efc1b87cf2c87145c373c1
Ree9o <ree9.business@gmail.com>
2024-03-23 23:13:30 +0900

    [add] login/ として切り離しを行った

---
+++ src/app/login/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function Login() {
+  const { data: session } = useSession();
+  return (
+    <>
+      <div className="flex justify-center">
+        {session ? (
+          <button
+            className=" flex w-full justify-center border-2 border-b border-red-300 hover:border-red-400 bg-red-200 hover:bg-red-300 pb-6 pt-8 backdrop-blur-2xl dark:border-red-800 dark:hover:border-red-900 dark:bg-red-800/50 dark:hover:bg-red-900/30 lg:static lg:w-auto lg:rounded-xl lg:p-4"
+            onClick={() => signOut()}
+          >
+            sign out
+          </button>
+        ) : (
+          <button
+            className=" flex w-full justify-center border-2 border-b border-red-300 hover:border-red-400 bg-red-200 hover:bg-red-300 pb-6 pt-8 backdrop-blur-2xl dark:border-red-800 dark:hover:border-red-900 dark:bg-red-800/50 dark:hover:bg-red-900/30 lg:static lg:w-auto lg:rounded-xl lg:p-4"
+            onClick={() => signIn("google", { callbackUrl: "/" })}
+          >
+            sign in
+          </button>
+        )}
+      </div>
+    </>
+  );
+}

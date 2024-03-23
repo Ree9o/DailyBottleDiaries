06231419f44539ac149b683ae13608e09250ef84
Ree9o <ree9.business@gmail.com>
2024-03-23 21:38:14 +0900

    [add]Googleアカウントでのログインを可能にしました。`signIn`と`signOut`関数を使用して、ログインとログアウト機能を実装しました。
    関連ファイル:
    - `src/app/api/auth/[...nextauth]/route.ts`
    - `src/app/page.tsx`
    - `src/providers/NextAuth.tsx`
    - `prisma/schema.prisma`
    - `prisma/migrations/*`

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };

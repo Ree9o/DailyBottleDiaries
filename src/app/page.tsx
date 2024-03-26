d57852c5aa75b93da2c6a27b5d80b107e3f6439a
Ree9o <ree9.business@gmail.com>
2024-03-26 20:12:44 +0900

    [REFACTOR]コンポーネント作成に合わせてリファクタ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,16 @@
+import DiariesContainer from "./_components/template/DiariesContainer";
 import { getServerSession } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import getDiaries from "./services/getDialies";
-import Daiarypaper from "./_component/molecules/Diarypaper/page";
 
 export default async function Page() {
   const session = await getServerSession();
-  // if (!session) {
-  //   redirect("/login");
-  // }
+  if (!session) {
+    redirect("/login");
+  }
 
-  const diaries = await getDiaries();
-  console.log(diaries);
   return (
     <>
-      <div className=" justify-center w-full h-screen">
-        {/* <div>こんにちは {session.user?.name}さん</div> */}
-        {diaries.map((diary) => (
-          <div key={diary.id}>
-            <Daiarypaper diaryData={diary} name={diary.user.name} />
-          </div>
-        ))}
-      </div>
+      <DiariesContainer />
     </>
   );
 }

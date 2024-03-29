58585bce9b75034e80086c7f1e77d6e513481922
Ree9o <ree9.business@gmail.com>
2024-03-29 18:32:46 +0900

    [add]クリエイト関数

---
+++ src/app/services/createDiary/index.ts
@@ -0,0 +1,26 @@
+"use server";
+import { prismaInitalied as prisma } from "@/src/lib/PrismaInitialized";
+
+interface CreateDiaryProps {
+  userId: string;
+  title: string;
+  content: string;
+  isPublic: boolean;
+}
+
+export default async function createDiary({ userId, title, content, isPublic }: CreateDiaryProps) {
+  const diaries = await prisma.diary.create({
+    data: {
+
+      title: title,
+      content: content,
+      isPublic: isPublic,
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+  return diaries;
+}

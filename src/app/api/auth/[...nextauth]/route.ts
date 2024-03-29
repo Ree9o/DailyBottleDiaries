0fba469f564dbf72a3724a353d4c7900005fcfd3
Ree9o <ree9.business@gmail.com>
2024-03-29 18:32:19 +0900

    [FIX]prismaクライアントをinitalizeしたものをexportして使うようにした

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import type { NextAuthOptions } from "next-auth";
-
+import { prismaInitalied as prisma } from "@/src/lib/PrismaInitialized";
 import { getServerSession as originalGetServerSession } from "next-auth";
 import { cache } from "react";
-const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
